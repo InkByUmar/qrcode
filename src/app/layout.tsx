b5bdd1d66e314c3d940a4e26a673fa45753e44a3
Umar Farooq <hme465436@gmail.com>
2026-05-12 08:17:38 +0000

    add social bar
    <script src="https://archaicmsflip.com/f8/d2/33/f8d2333

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,6 +95,11 @@ export default function RootLayout({
           src="https://cdn.jsdelivr.net/npm/qr-code-styling@1.9.2/lib/qr-code-styling.min.js" 
           strategy="afterInteractive"
         />
+        {/* Adsterra Social Bar */}
+        <Script 
+          src="https://archaicmsflip.com/f8/d2/33/f8d2333ae12f97dc648dbb943b8bf90d.js"
+          strategy="afterInteractive"
+        />
       </head>
       <body 
         className="font-body bg-[#060907] text-foreground antialiased selection:bg-primary selection:text-primary-foreground"
